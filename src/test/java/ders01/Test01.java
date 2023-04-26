5efb55edf851372116e8519211d013cfb2130226
hanifi <hanifi2769@gmail.com>
2023-04-26 18:33:45 +0300

    apiiii

---
+++ src/test/java/ders01/Test01.java
@@ -0,0 +1,27 @@
+package ders01;
+
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test01 {
+
+    @Test
+    public void test01(){
+
+        String url="https://restful-booker.herokuapp.com/booking";
+
+        Response response= given().when().get(url);
+
+        response.prettyPrint();
+
+        System.out.println("Status Code : " + response.statusCode());
+        System.out.println("Content Type : " + response.contentType());
+        System.out.println("Test Zamanı : " +response.time());
+
+        Assert.assertEquals(200,response.statusCode());
+        Assert.assertEquals("application/json; charset=utf-8",response.contentType());
+    }
+}
