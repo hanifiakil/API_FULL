506acf6e880ed1a47723414bc8b831f354986fb8
hanifi <hanifi2769@gmail.com>
2023-04-29 17:22:59 +0300

    ahmet

---
+++ src/test/java/ders01/GetRequest06.java
@@ -0,0 +1,24 @@
+package ders01;
+
+import base_url.ReqresinBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetRequest06 extends ReqresinBaseUrl {
+
+    @Test
+    public void test06(){
+
+spec01.pathParams("parametre1","api","parametre2","users");
+
+        Response response=given().spec(spec01).when().get("/{parametre1}/{parametre2}");
+
+        response.prettyPrint();
+//Matchers Class
+        response.then().assertThat().body("data[4].email",equalTo("charles.morris@reqres.in"),
+                                           "data[4].first_name",equalTo("Charles"));
+    }
+}
