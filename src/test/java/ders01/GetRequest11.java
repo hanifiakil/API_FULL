9f476325044749e3e4e3301b76e0855e3d9a4780
hanifi <hanifi2769@gmail.com>
2023-05-03 17:20:17 +0300

    api

--- src/test/java/ders01/GetRequest11.java
+++ src/test/java/ders01/GetRequest11.java
@@ -1,10 +1,14 @@
 package ders01;
 
 import base_url.GMIBankBaseUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetRequest11 extends GMIBankBaseUrl {
 
@@ -21,10 +25,21 @@ public class GetRequest11 extends GMIBankBaseUrl {
 
         //MATCHERS CLASS İLE
 
-
-
+        response.then().assertThat().body("firstName", equalTo("Della"),
+                "lastName", equalTo("Heaney"),
+                "mobilePhoneNumber",equalTo("123-456-7893"),
+                "accounts[0].balance",equalTo(69700));
 
         //JSON PATH İLE
+
+        JsonPath json =response.jsonPath();
+
+        Assert.assertEquals("Della",json.getString("firstName"));
+        Assert.assertEquals("Heaney",json.getString("lastName"));
+        Assert.assertEquals(11190,json.getInt("accounts[1].balance"));
+
+
+
     }
 
 
