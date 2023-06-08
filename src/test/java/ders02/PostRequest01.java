fce84a36dbda3de201252d5743f889f5068f7ec9
hanifi <hanifi2769@gmail.com>
2023-06-08 18:24:31 +0300

    yeni

--- src/test/java/ders02/PostRequest01.java
+++ src/test/java/ders02/PostRequest01.java
@@ -2,8 +2,10 @@ package ders02;
 
 import base_url.HerokuappBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 import test_data.HerokuappTestData;
 
@@ -40,6 +42,22 @@ public class PostRequest01 extends HerokuappBaseUrl {
                           post("/{bir}");
 
         response.prettyPrint();
+
+        //JSONObject te toString() kullanmalıyız   .body(expectedRequestData.toString())
+
+        //4)doğrulama
+        // JSON PATH
+
+        JsonPath json =response.jsonPath();
+        response.then().assertThat().statusCode(200);
+        // TEST DATA İÇERİSİNDEKİ firstname                     , bady deki firstname
+        Assert.assertEquals(expectedRequestData.getString("firstname"),json.getString("booking.firstname"));
+        Assert.assertEquals(expectedRequestData.getString("lastname"),json.getString("booking.lastname"));
+        Assert.assertEquals(expectedRequestData.getInt("totalprice"),json.getInt("booking.totalprice"));
+        Assert.assertEquals(expectedRequestData.getBoolean("depositpaid"),json.getBoolean("booking.depositpaid"));
+
+        Assert.assertEquals(expectedRequestData.getJSONObject("bookingdates").getString("checkin"),json.getString("booking.bookingdates.checkin"));
+        Assert.assertEquals(expectedRequestData.getJSONObject("bookingdates").getString("checkout"),json.getString("booking.bookingdates.checkout"));
 
     }
 }
